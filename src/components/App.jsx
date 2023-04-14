29121f31b8ecef7c4cfe09763b4df25ee133f217
Jamie Catto <jamietcatto@gmail.com>
2023-04-14 17:00:50 +0200

    Added component structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
+import { Actors, Movies, Profile, MovieInformation, Navbar } from '.';
 
 const App = () => (
   <div>
     <CssBaseline />
+    <Navbar />
     <main>
       <Routes>
         <Route path="/" element=<h1>Home</h1> />
-        <Route exact path="/movie/:id" />
-        <Route exact path="/actors/:id" />
-        <Route exact path="/movies" />
-        <Route exact path="/profile/:id" />
+        <Route exact path="/movie/:id" element={MovieInformation()} />
+        <Route exact path="/actors/:id" element={Actors()} />
+        <Route exact path="/movies" element={Movies()} />
+        <Route exact path="/profile/:id" element={Profile()} />
       </Routes>
     </main>
   </div>
