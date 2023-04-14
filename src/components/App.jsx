c926fdf6a3909f1a5dca83792e21d158a9748cf0
Jamie Catto <jamietcatto@gmail.com>
2023-04-14 22:14:24 +0200

    Added ability to style through js file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import { Actors, Movies, Profile, MovieInformation, Navbar } from '.';
+import useStyles from './styles';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
-    <main>
-      <Routes>
-        <Route path="/" element=<h1>Home</h1> />
-        <Route exact path="/movie/:id" element={MovieInformation()} />
-        <Route exact path="/actors/:id" element={Actors()} />
-        <Route exact path="/movies" element={Movies()} />
-        <Route exact path="/profile/:id" element={Profile()} />
-      </Routes>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="/" element=<h1>Home</h1> />
+          <Route exact path="/movie/:id" element={MovieInformation()} />
+          <Route exact path="/actors/:id" element={Actors()} />
+          <Route exact path="/movies" element={Movies()} />
+          <Route exact path="/profile/:id" element={Profile()} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
