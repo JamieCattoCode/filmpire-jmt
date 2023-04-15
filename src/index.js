d2ac2c7ee94a5a216c65b85b26d609f9eb8c6fd5
Jamie Catto <jamietcatto@gmail.com>
2023-04-15 11:39:03 +0200

    Added navbar without functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
 
+const theme = createTheme({});
+
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
