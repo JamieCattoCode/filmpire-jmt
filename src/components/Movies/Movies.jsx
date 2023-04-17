af825d0ae6ff56ee8349edd4cf4271ca9fd18905
Jamie Catto <jamietcatto@gmail.com>
2023-04-17 16:37:52 +0200

    Added view of movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import MovieList from '../MovieList/MovieList';
+
+import { useGetMoviesQuery } from '../../services/TMBD';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies found with that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'Error occurred';
+
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 };
